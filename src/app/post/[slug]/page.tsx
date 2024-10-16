2ded48e34de41f966310986ffd5f0c37c865af92
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-16 13:49:41 +0200

    create postpage

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,1 +1,35 @@
-// post page
+import { Card, CardBody, CardHeader, Image } from '@nextui-org/react'
+import { notFound } from 'next/navigation'
+
+import { createClient } from '@/app/utils/supabase/client'
+
+export default async function PostPage({ params }:{ params: { slug: string }}) {
+  const supabase = createClient()
+  const {data, error} = await supabase
+    .from('posts')
+    .select('title, content, users("username")')
+    .eq('slug', params.slug)
+    .single()
+
+    if (!data || error) notFound()
+
+      console.log(data)
+  return (
+    <Card className="py-4">
+    <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+      <p className="text-tiny uppercase font-bold">{data.users?.username}</p>
+      {/* <small className="text-default-500">{data.timestampz}</small> // fix timestamp */}
+      <h4 className="font-bold text-large">{data.title}</h4>
+    </CardHeader>
+    <CardBody className="overflow-visible py-2">
+      <Image
+        alt="Card background"
+        className="object-cover rounded-xl"
+        src="https://nextui.org/images/hero-card-complete.jpeg" // fix actual imagesrc
+        width={270}
+      />
+      <p>{data.content}</p>
+    </CardBody>
+  </Card>
+  )
+}
