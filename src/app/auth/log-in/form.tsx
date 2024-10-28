03946dc32d3564c90771b4b4fe401ee51acc6802
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-28 12:51:28 +0100

    add name in login form to make login action work

--- src/app/auth/log-in/form.tsx
+++ src/app/auth/log-in/form.tsx
@@ -1,15 +1,19 @@
+'use client'
+
 import { logIn } from "@/actions/log-in";
 import { Button, Input } from "@nextui-org/react";
+import { useState } from "react";
 
 export const LogInForm = () => {
+
   return (
     <form
       action={logIn}
       className="flex w-full max-w-md flex-col gap-4"
     >
       <div className="flex flex-col gap-4 items-center mx-4">
-        <Input type="email" label="Email" /> {/* or use username? */}
-        <Input type="password" label="Password" />
+        <Input type="email" name="email" label="Email" /> {/* or use username? */}
+        <Input type="password" name="password" label="Password" />
         <Button type="submit" size="sm">
           Log in
         </Button>
