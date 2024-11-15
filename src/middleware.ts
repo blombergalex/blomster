7d1f078d6a0e6c102b3d2735321015555bd1aa27
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-11-15 10:17:23 +0100

    remove blank space

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
-import { createServerClient } from "@supabase/ssr"
-import { NextResponse, NextRequest } from "next/server"
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, NextRequest } from "next/server";
 
 export const middleware = async (request: NextRequest) => {
-  let supabaseResponse = NextResponse.next({request})
+  let supabaseResponse = NextResponse.next({ request });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -10,37 +10,36 @@ export const middleware = async (request: NextRequest) => {
     {
       cookies: {
         getAll() {
-          return request.cookies.getAll()
+          return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({name, value}) =>
-          request.cookies.set(name, value),
-        )
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value)
+          );
 
-        supabaseResponse = NextResponse.next({request})
-        cookiesToSet.forEach(({name, value, options}) => 
-          supabaseResponse.cookies.set(name, value, options))
+          supabaseResponse = NextResponse.next({ request });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
         },
-      }
+      },
     }
-  )
+  );
 
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
-  if (!user && protectedRoutes.some((route) => route.test(request.nextUrl.pathname))) {
-    const url = request.nextUrl.clone()
-    url.pathname = '/auth/log-in'
-    return NextResponse.redirect(url)
+  if (
+    !user &&
+    protectedRoutes.some((route) => route.test(request.nextUrl.pathname))
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/auth/log-in";
+    return NextResponse.redirect(url);
   }
 
-  
+  return supabaseResponse;
+};
 
-  return supabaseResponse
-}
-
-const protectedRoutes = [
-  /^\/create$/,
-  /^\/post\/[^\/]+\/edit$/
-]
+const protectedRoutes = [/^\/create$/, /^\/post\/[^\/]+\/edit$/];
