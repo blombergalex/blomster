ecf50443f14caff666181c0c04a3e2f1d94f03db
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-11-20 09:08:49 +0100

    create HOF and try to use it in log-in action

--- src/actions/log-in.ts
+++ src/actions/log-in.ts
@@ -5,17 +5,41 @@ import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
 import { logInSchema } from './schemas'
+import { createServerAction, ServerActionError } from '@/utils/action-utils'
 
-export const logIn = async (data: z.infer<typeof logInSchema>) => {
+export const logIn = createServerAction(async (data: z.infer<typeof logInSchema>) => {
   const supabase = createClient()
 
   const parsedData = logInSchema.parse(data)
 
   const {error} = await supabase.auth.signInWithPassword(parsedData)
   if (error) {
-    throw error
+    console.log("error: ", error?.message)
+    throw new ServerActionError(error.message || "Failed to login")
   }
 
   redirect('/')  
-}
+})
 
+// original: 
+
+// 'use server'
+
+// import { z } from 'zod'
+// import { redirect } from 'next/navigation'
+
+// import { createClient } from '@/utils/supabase/server'
+// import { logInSchema } from './schemas'
+
+// export const logIn = async (data: z.infer<typeof logInSchema>) => {
+//   const supabase = createClient()
+
+//   const parsedData = logInSchema.parse(data)
+
+//   const {error} = await supabase.auth.signInWithPassword(parsedData)
+//   if (error) {
+//     throw error
+//   }
+
+//   redirect('/')  
+// }
