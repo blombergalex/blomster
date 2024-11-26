74cbb7312fbece5fba3f5e5f288ed6a00762448a
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-11-26 16:43:04 +0100

    tidy code

--- src/actions/log-in.ts
+++ src/actions/log-in.ts
@@ -17,32 +17,8 @@ export const logIn = async (
   const { error } = await supabase.auth.signInWithPassword(parsedData);
   if (error) {
     console.error("error: ", error?.message);
-    // throw new ServerActionError(error.message || "Failed to login")
     return { error: error.message};
   }
 
   redirect("/");
 };
-
-// original:
-
-// 'use server'
-
-// import { z } from 'zod'
-// import { redirect } from 'next/navigation'
-
-// import { createClient } from '@/utils/supabase/server'
-// import { logInSchema } from './schemas'
-
-// export const logIn = async (data: z.infer<typeof logInSchema>) => {
-//   const supabase = createClient()
-
-//   const parsedData = logInSchema.parse(data)
-
-//   const {error} = await supabase.auth.signInWithPassword(parsedData)
-//   if (error) {
-//     throw error
-//   }
-
-//   redirect('/')
-// }
