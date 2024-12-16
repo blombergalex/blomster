99cab0a8bb0821f72d48ae50c90983bbef7a2d5d
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-12-16 12:47:16 +0100

    add delete functionality to mobile button

--- src/components/delete-comment-button.tsx
+++ src/components/delete-comment-button.tsx
@@ -5,7 +5,7 @@ import { deleteComment } from "@/actions/delete-comment";
 import { Button } from "@nextui-org/react";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 export const DeleteCommentButton = ({
   commentId,
@@ -23,16 +23,16 @@ export const DeleteCommentButton = ({
   });
 
   return (
-    <div className="absolute right-1 top-1"> 
-    <Button
-      className="hidden m-3 sm:flex p-1"
-      onClick={() => mutate()}
-      >
-      Delete
-    </Button>
-    <div className="font-semibold m-2 ">
-    <XMarkIcon className="h-6 w-6 text-primary-500 sm:hidden"/>
-    </div>
+    <div className="absolute right-1 top-1">
+      <Button className="hidden m-3 sm:flex p-1" onClick={() => mutate()}>
+        Delete
+      </Button>
+      <div className="font-semibold m-2 ">
+        <XMarkIcon
+          className="h-6 w-6 text-primary-500 sm:hidden"
+          onClick={() => mutate()}
+        />
+      </div>
     </div>
   );
 };
